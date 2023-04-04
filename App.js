dfd47b1722fb0a4a36e7b187412d025b848d7e44
seleena-P <108916473+seleena-P@users.noreply.github.com>
2023-04-05 02:44:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import Smile from './Smile';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Smile/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Smile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+//import smilee from './Smilee.png'
+function Smile() {
+  return (
+    <div>
+        <h1>Smile Component</h1>
+        <p>It is a functional component</p>
+        <img src="https://png.pngtree.com/png-vector/20190217/ourmid/pngtree-smile-vector-template-design-illustration-png-image_555090.jpg" alt=""></img>
+    </div>
+  )
+}
+
+export default Smile
